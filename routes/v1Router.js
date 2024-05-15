55c19cf105526d9afc8093cd31818d369dcd57fa
Adrian <adferrer0611@gmail.com>
2024-05-15 19:12:51 -0400

    Problems with searchFood, it seems the endpoint is not hitted when requesting it

--- routes/v1Router.js
+++ routes/v1Router.js
@@ -1,6 +1,10 @@
 const v1Router = require("express").Router();
 const categoryRouter = require("./category");
+const restaurantRouter = require("./restaurants");
+const foodRouter = require("./foods");
 
 v1Router.use("/categories", categoryRouter);
+v1Router.use("/restaurants", restaurantRouter);
+v1Router.use("/foods", foodRouter);
 
 module.exports = v1Router;
--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const port = PORT || 6013;
 connect(MONGO_URL);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
 
 app.use("/api/v1", v1Router);
 
